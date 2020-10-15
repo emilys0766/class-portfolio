043e5a2df9c420283ad94f34a3fb69abad540e5a
emilys0766 <emilys0766@hstat.org>
2020-10-15 17:00:28 -0400

    Update sketch.js

--- logo/sketch.js
+++ logo/sketch.js
@@ -1,10 +1,10 @@
 
-function preload() {
-    var catImage
-    var catPic
-   catImage = loadImage ('thinking.jpg')
-   catPic = loadImage ('smugcat.jpg')
-}
+//function preload() {
+ //   var catImage
+  //  var catPic
+//   catImage = loadImage ('thinking.jpg')
+//   catPic = loadImage ('smugcat.jpg')
+//}
 
 function setup() {
     createCanvas(640, 360);
@@ -14,36 +14,37 @@ function setup() {
 }
 
 function draw() {
+     background('pink');
+
+    //image(catImage, 100,100);
+    stroke(0);
+    strokeWeight(2)
+
+    textSize(20);
+    fill(255);
+    stroke('black');
+    strokeWeight(5);
+    textFont('impact');
+
+    textAlign(CENTER);
+
+    text("When youre thinking about ways to get your owner's attention...", width / 2, height / 6);
+        if (keyIsPressed === true) {
     background('pink');
 
-    image(catImage, 100,100);
+    //image(catPic, 100,100);
     stroke(0);
     strokeWeight(2)
 
-    textSize(80);
-    fill('aqua');
-    stroke(255);
+    textSize(20);
+    fill(255);
+    stroke('black');
     strokeWeight(5);
     textFont('impact');
 
     textAlign(CENTER);
 
-    text('Cat', width / 2, height / 5);
-
-    if (keyIsPressed === true) {
-    image(catImage, 100,100);
-    stroke(0);
-    strokeWeight(2)
-
-    textSize(80);
-    fill('aqua');
-    stroke(255);
-    strokeWeight(5);
-    textFont('impact');
-
-    textAlign(CENTER);
-
-    text('Cat', width / 2, height / 5);
+    text("And you see the snack drawer is open...", width / 2, height / 6);
   } else {
   }
 }
