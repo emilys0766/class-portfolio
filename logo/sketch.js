f2fddc5f4c777c8659e02756223c68a3b2e9424a
emilys0766 <emilys0766@hstat.org>
2020-10-20 18:13:19 -0400

    Update sketch.js

--- logo/sketch.js
+++ logo/sketch.js
@@ -1,10 +1,9 @@
-
-// function preload() {
-//    var catImage
-//    var catPic
-//   catImage = loadImage ('thinking.jpg')
-//   catPic = loadImage ('smugcat.jpg')
-// }
+   var catImage
+   var catPic
+function preload() {
+  catImage = loadImage ('thinking.jpg')
+  catPic = loadImage ('smugcat.jpg')
+}
 
 function setup() {
     createCanvas(640, 360);
@@ -16,7 +15,7 @@ function setup() {
 function draw() {
      background('pink');
 
-    // image(catImage, 100,100);
+    image(catImage, 100,100);
     stroke(0);
     strokeWeight(2)
 
@@ -32,7 +31,7 @@ function draw() {
         if (keyIsPressed === true) {
     background('pink');
 
-    // image(catPic, 100,100);
+    image(catPic, 100,100);
     stroke(0);
     strokeWeight(2)
 
