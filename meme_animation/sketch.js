84c63b84fd341549c9b364f8ff59f3a8a6b366bf
emilys0766 <emilys0766@hstat.org>
2020-10-22 17:27:23 -0400

    Update sketch.js

--- meme_animation/sketch.js
+++ meme_animation/sketch.js
@@ -1,19 +1,21 @@
-
-//var catImage;
-
-// function preload() {
-//     catImage = loadImage ('thinking.jpg');
-// }
+  var catImage
+   var catPic
+function preload() {
+  catImage = loadImage ('thinking.jpg')
+  catPic = loadImage ('smugcat.JPG')
+}
 
 function setup() {
-    var canvas = createCanvas(640, 360);
-    canvas.drawingContext.miterLimit = 2;
+    createCanvas(640, 360);
+    var p = color ('aqua');
+   // console.log(p);
+  //  canvas.drawingContext.miterLimit = 2;
 }
 
 function draw() {
     background('pink');
+    image(catImage, 0,0,width,height);
 
-    //image(catImage, 100,100);
     stroke(0);
     strokeWeight(2)
 
@@ -23,14 +25,18 @@ function draw() {
     strokeWeight(5);
     textFont('impact');
 
-    textAlign(CENTER);
+    textAlign(CENTER, TOP);
 
-    text("When youre thinking about ways to get your owner's attention...", width / 2, height / 6);
+    text("When you're thinking about what you should have for dinner...", width / 2, height / 6);
+
+  stroke(50);
+  fill(100);
+  ellipse(x, y, 24, 24);
+    
         if (keyIsPressed === true) {
-    //image(catImage, 100,100);
- background('pink');
+    background('pink');
 
-    //image(catImage, 100,100);
+    image(catPic, 0,0,width,height);
     stroke(0);
     strokeWeight(2)
 
@@ -40,9 +46,9 @@ function draw() {
     strokeWeight(5);
     textFont('impact');
 
-    textAlign(CENTER);
+    textAlign(CENTER, TOP);
 
-    text("And you see the snack drawer is openccc", width / 2, height / 6);
+    text("And you see the snack drawer is open...", width / 2, height / 6);
   } else {
   }
 }
