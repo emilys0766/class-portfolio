631172d2febc7aaa2c1998ff95bad683d829ad14
emilys0766 <emilys0766@hstat.org>
2020-10-29 17:24:17 -0400

    new file

---
+++ meme_sound/sketch.js
@@ -0,0 +1,56 @@
+   var catImage
+   var catPic
+   var s = 500
+function preload() {
+  catImage = loadImage ('thinking.jpg')
+  catPic = loadImage ('smugcat.JPG')
+}
+
+function setup() {
+    createCanvas(640, 360);
+    var p = color ('aqua');
+   // console.log(p);
+  //  canvas.drawingContext.miterLimit = 2;
+}
+
+function draw() {
+    background('pink');
+    image(catImage, width-s, height-s);
+    s += 2;
+
+    if (catImage.width > canvas.width) {
+        s -= 2
+    }
+
+
+    stroke(0);
+    strokeWeight(2)
+
+    textSize(20);
+    fill(255);
+    stroke('black');
+    strokeWeight(5);
+    textFont('impact');
+
+    textAlign(CENTER, TOP);
+
+    text("When you're thinking about what you should have for dinner...", width / 2, height / 6);
+        if (keyIsPressed === true) {
+    background('pink');
+
+    image(catPic, 0,0,width,height);
+    stroke(0);
+    strokeWeight(2)
+
+    textSize(20);
+    fill(255);
+    stroke('black');
+    strokeWeight(5);
+    textFont('impact');
+
+    textAlign(CENTER, TOP);
+
+    text("And you see the snack drawer is open...", width / 2, height / 6);
+  } else {
+  }
+}
