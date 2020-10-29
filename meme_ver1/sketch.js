be7a8422a8a7e7c0f50081593ad7d159114fedcb
emilys0766 <emilys0766@hstat.org>
2020-10-29 18:00:07 -0400

    added meme

---
+++ meme_ver1/sketch.js
@@ -0,0 +1,49 @@
+   var catImage
+   var catPic
+function preload() {
+  catImage = loadImage ('thinking.jpg')
+  catPic = loadImage ('smugcat.JPG')
+}
+
+function setup() {
+    createCanvas(640, 360);
+    var p = color ('aqua');
+   // console.log(p);
+  //  canvas.drawingContext.miterLimit = 2;
+}
+
+function draw() {
+    background('pink');
+    image(catImage, 0,0,width,height);
+
+    stroke(0);
+    strokeWeight(2)
+
+    textSize(20);
+    fill(255);
+    stroke('black');
+    strokeWeight(5);
+    textFont('impact');
+
+    textAlign(CENTER, TOP);
+
+    text("When you're thinking about what you should have for dinner...", width / 2, height / 6);
+        if (keyIsPressed === true) {
+    background('pink');
+
+    image(catPic, 0,0,width,height);
+    stroke(0);
+    strokeWeight(2)
+
+    textSize(20);
+    fill(255);
+    stroke('black');
+    strokeWeight(5);
+    textFont('impact');
+
+    textAlign(CENTER, TOP);
+
+    text("And you see the snack drawer is open...", width / 2, height / 6);
+  } else {
+  }
+}
