2562c14b42ce11e4c7d950d18d1c3c7c63413d3f
emilys0766 <emilys0766@hstat.org>
2020-12-20 18:05:34 -0500

    landscape 1

--- landscapes_1/sketch.js
+++ landscapes_1/sketch.js
@@ -4,22 +4,38 @@ function setup() {
 
 function draw() {
   background(225);
-  
-  for (let x = 0; x < width; x+=100)
-  
-  fill('grey')
-  rect(0, 100, 100, 400)
-  fill('yellow')
-  rect(10, 125, 25)
-  rect(60, 125, 25)
-  // rect(10, 175, 25)
-  // rect(60, 175, 25)
-  // rect(10, 225, 25)
-  // rect(60, 225, 25)
-  // rect(10, 275, 25)
-  // rect(60, 275, 25)
-  // rect(10, 325, 25)
-  // rect(60, 325, 25)
-  // rect(10, 375, 25)
-  // rect(60, 375, 25)
+
+  for (let x = 0; x < width; x += 120) {
+    noStroke();
+    fill(80, 80, x);
+    rect(x, height - 300, 100, 400);
+  }
+
+  for (let x2 = 0; x2 < width; x2 += 120) {
+     fill('yellow')
+     rect(x2 + 15, height - 275, 25, 25)
+     rect(x2 - 60, height - 275, 25, 25)
+     rect(x2 + 15, height - 225, 25, 25)
+     rect(x2 - 60, height - 225, 25, 25)
+     rect(x2 + 15, height - 175, 25, 25)
+     rect(x2 - 60, height - 175, 25, 25)
+     rect(x2 + 15, height - 125, 25, 25)
+     rect(x2 - 60, height - 125, 25, 25)
+     rect(x2 + 15, height - 75, 25, 25)
+     rect(x2 - 60, height - 75, 25, 25)
+     rect(x2 + 15, height - 25, 25, 25)
+     rect(x2 - 60, height - 25, 25, 25)
+  }
+
+ for (let w2 = 0; w2 < width; w2 += 120) {
+     fill("white")
+     stroke("white")
+     ellipse(w2, 30, 50, 50)
+     ellipse(w2+20, 40, 50, 50)
+     ellipse(w2+40, 30, 50, 50)
+            
+     ellipse(w2+100, 40, 50, 50)
+     ellipse(w2+120, 30, 50, 50)
+     ellipse(w2+140, 40, 50, 50)
+          }
 }
